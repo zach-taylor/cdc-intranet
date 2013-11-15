b9abd431b7f93391a97f7ac1e8aad88bf00a0d4d
Zach Taylor <ztaylor234@gmail.com>
2013-11-15 13:04:04 -0600

    it no likey

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = 'intranet'
+Rails.application.config.secret_token = 'c9bab17cf5682b0fbe07ee1e2f21eea0'
